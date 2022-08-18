6e7ba926fe02e3abd1463299abcd7f6333c477e3
Olive <olivelliott48@gmail.com>
2022-08-18 18:12:48 -0400

    Configure lazy loading

---
+++ assets/js/domMethods.js
@@ -0,0 +1,36 @@
+require('bootstrap');
+const createEl = require('./domMethods');
+
+function createEl(htmlString, attrs, ...children) {
+    if (typeof htmlString !== "string") {
+      throw Error("Argument 'htmlString' is required and must be a string");
+    }
+  
+    const el = document.createElement(htmlString);
+  
+    if (typeof attrs === "object") {
+      for (let key in attrs) {
+        if (key.substring(0, 2) === "on") {
+          el.addEventListener(key.substring(2).toLowerCase(), attrs[key]);
+        } else {
+          el.setAttribute(key, attrs[key]);
+        }
+      }
+    }
+  
+    children.forEach(function(child) {
+      let node;
+  
+      if (child.constructor.name.includes("Element")) {
+        node = child;
+      } else {
+        node = document.createTextNode(child);
+      }
+  
+      el.appendChild(node);
+    });
+  
+    return el;
+  };
+
+module.exports = createEl;
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,26 @@
 const path = require('path');
 const webpack = require('webpack');
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
-    entry: './assets/js/script.js',
+    entry: {
+        app: './assets/js/script.js',
+        events: './assets/js/events.js',
+        schedule: './assets/js/schedule.js',
+        tickets: './assets/js/tickets.js'
+    },
     output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'main.bundle.js',
+        path: path.resolve(__dirname, 'dist/'),
+        filename: '[name].bundle.js',
     },
     plugins: [
         new webpack.ProvidePlugin({
             $: 'jquery',
             jQuery: 'jquery'
+        }),
+        new BundleAnalyzerPlugin({
+            analyzerMode: 'static' 
+            // this will output an HTML file in dist folder
         })
     ],
     mode: 'development'
