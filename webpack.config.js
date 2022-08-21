ca5ca72d57bc066d41928f73a7ecdd0d20498b0e
Olive <olivelliott48@gmail.com>
2022-08-21 13:19:30 -0400

    Convert into a PWA

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
@@ -45,6 +46,21 @@ module.exports = {
         new BundleAnalyzerPlugin({
             analyzerMode: 'static' 
             // this will output an HTML file in dist folder
+        }),
+        new WebpackPwaManifest({
+            name: 'Food Event',
+            short_name: 'Foodies',
+            description: 'An app that allows you to view upcoming food events',
+            start_url: '../index.html',
+            background_color: '#01579b',
+            theme_color: '#fff',
+            fingerprints: false,
+            inject: false,
+            icons: [{
+                src: path.resolve('assets/img/icons/icon-512x512.png'),
+                sizes: [96, 128, 192, 256, 384, 512],
+                destination: path.join('assets', 'icons')
+            }]
         })
     ],
     mode: 'development'
